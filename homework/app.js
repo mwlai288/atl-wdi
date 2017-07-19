28d28f6a1cdcecdc0de95b5e6720ca4f23843863
mwlai288 <mwlai288@gmail.com>
2017-07-19 17:33:10 -0400

    completed GET routes

--- homework/app.js
+++ homework/app.js
@@ -6,6 +6,21 @@ var app     = express();
 var port    = 3000;
 
 
+app.get('/topping/olives', (req, res) => {
+
+    res.send('Olives, are gross');
+});
+
+
+app.get('/order/:amount/:size', (req, res) => {
+
+     res.send(`We would like ${req.params.amount} ${req.params.size} small pizzas now`);
+ });
+
+
+
+
+
 app.listen(port, function(){
   console.log("==========================")
   console.log('LISTENING ON PORT ' + port);
