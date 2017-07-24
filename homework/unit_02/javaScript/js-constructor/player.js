19b1698f8f5e08d41888006e579265d7ee9522e1
mwlai288 <mwlai288@gmail.com>
2017-07-24 18:17:59 -0400

    part 4: added new methods

--- homework/unit_02/javaScript/js-constructor/player.js
+++ homework/unit_02/javaScript/js-constructor/player.js
@@ -2,15 +2,15 @@ console.log('player file is loaded');
 
 var player = {
   name: "Neo",
-  feedTamagotchi: function(tamagotchi) {
+  feedTamagotchi: function (tamagotchi) {
     tamagotchi.foodInTummy++;
     console.log('YUM!!! ' + tamagotchi.name + ' has been fed and now has: ' + tamagotchi.foodInTummy + ' in the tummy');
   },
-  medicateTamagotchi: function(tamagotchi) {
+  medicateTamagotchi: function (tamagotchi) {
     tamagotchi.health++;
     console.log(tamagotchi.name + ' has been medicated and now has health: ' + tamagotchi.health);
   },
-  knockOutTamagotchi: function(tamagotchi) {
+  knockOutTamagotchi: function (tamagotchi) {
     tamagotchi.restedness++;
     console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
   }
--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -2,31 +2,50 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 class Tamagotchi {
-  constructor(name, creatureType){
-    this.name = name;
-    this.creatureType = creatureType;
-    this.foodInTummy = 10;
-    this.restedness = 10;
-    this.health = 10;
-  }
+    constructor(name, creatureType) {
+        this.name = name;
+        this.creatureType = creatureType;
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+    }
 
-  cry() {
-    this.foodInTummy--;
-    console.log("WAAAH!!!!!!");
-    console.log(this.name + ' has current food in tummy = ' + this.foodInTummy);
-  };
-  puke() {
-    this.health--;
-    console.log('Bleeeeh! Sick!!!!!!');
-    console.log(this.name + ' has current health = ' + this.health);
-  };
+    cry() {
+        this.foodInTummy--;
+        console.log("WAAAH!!!!!!");
+        console.log(this.name + ' has current food in tummy = ' + this.foodInTummy);
+    };
+    puke() {
+        this.health--;
+        console.log('Bleeeeh! Sick!!!!!!');
+        console.log(this.name + ' has current health = ' + this.health);
+    };
 
-  yawn(){
-    this.restedness--;
-    console.log('Yaaaawwwwn! So tired');
-    console.log(this.name + ' has current restedness = ' + this.restedness);
-  };
-}   
+    yawn() {
+        this.restedness--;
+        console.log('Yaaaawwwwn! So tired');
+        console.log(this.name + ' has current restedness = ' + this.restedness);
+    };
+    start() {
+        this.hungerTimer = setInterval(function () {
+            name.cry();
+        }, 6000);
+        this.yawnTimer = setInterval(function () {
+            name.yawn();
+        }, 10000);
+        this.sickTimer = setInterval(function () {
+            name.puke();
+        }, 25000);
+    };
+    stop() {
+        console.log('Stop ' + this.name);
+        clearInterval(this.hungerTimer);
+        clearInterval(this.yawnTimer);
+        clearInterval(this.sickTimer);
+
+    };
+}
+
 
 
 
