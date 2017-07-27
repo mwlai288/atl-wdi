ee7a956838bacda9ea6a20630b67dbc7f842999c
mwlai288 <mwlai288@gmail.com>
2017-07-26 20:33:40 -0400

    created donut schema

--- homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,18 @@
 //requirements: require mongoose
+var mongoose = require("mongoose");
 
 
 //create your donut schema:
+var donutSchema = new Schema({
+//write your schema fields here
 
+    name: String,
+    description: String,
+    img: String,
+    price: Number,
+    qty: Number
+
+});
 
 //export your donut with module.exports()
+module.exports = donuts;
